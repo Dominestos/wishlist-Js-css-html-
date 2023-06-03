308c76d7693b1398933daa20f9e2b68a5df78047
Dominestos <dominik.sobolevsky@gmail.com>
2023-06-03 21:48:05 +0300

    js hero

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+import { renderNavigation } from "./renderNavigation.js";
+import { createHero } from "./createHero.js";
+const app = document.querySelector('.app');
+
+const handleHomePage = () => {
+  app.textContent = '';
+  renderNavigation();
+
+  const section = createHero();
+  app.append(createHero());
+}
+
+const init = () => {
+  handleHomePage();
+}
+
+init();
