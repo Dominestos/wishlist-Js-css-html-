08927b98470f86a09b42abf1ce918da4eaa62d5a
Dominestos <dominik.sobolevsky@gmail.com>
2023-08-10 12:57:41 +0300

    adaptivecheckup

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,60 @@
+import { JWT_TOKEN_KEY } from "./const.js";
 import { renderNavigation } from "./renderNavigation.js";
 import { createHero } from "./createHero.js";
+import { getLogin } from "./getLogin.js";
+
+export const router = Router();
+const token = localStorage.getItem(JWT_TOKEN_KEY);
+export const auth = token ? getLogin(token) : {};
+
 const app = document.querySelector('.app');
+
+
+
+
+const handleEditPageRoute = (id) => {
+
+};
+
+const handleEditProfileRoute = (login) => {
+
+};
+
+const handleUserRoute = async (login) => {
+  app.textContent = '';
+  renderNavigation();
+  app.append(await createWishlist(login));
+
+};
+
 
 const handleHomePage = () => {
   app.textContent = '';
   renderNavigation();
-
-  const section = createHero();
   app.append(createHero());
 }
 
 const init = () => {
-  handleHomePage();
+  let isMainPage = true;
+
+  router.on('/', handleHomePage);
+  router.on('/editwish/newwish', handleEditPageRoute);
+  router.on('/editwish/:id', handleEditPageRoute);
+  router.on('/editprofile/:login', handleEditProfileRoute);
+  router.on('/user/:login', handleUserRoute);
+
+  router.init();
+
+  if (isMainPage) {
+
+    isMainPage = false;
+
+    if (auth.login) {
+      router.setRoute(`/user/${auth.login}`);
+    }
+
+    router.setRoute('/')
+  }
 }
 
 init();
--- scripts/renderNavigation.js
+++ scripts/renderNavigation.js
@@ -1,6 +1,12 @@
+import { API_URL, JWT_TOKEN_KEY } from './const.js';
+import { createBurgerMenu } from './createBurgerMenu.js';
 import { createElement } from './helper.js';
+import { auth, router } from './index.js';
+import { renderModal } from './renderModal.js';
+
 const nav = document.querySelector('.nav');
-export const burger = createBurgerMenu(nav);
+createBurgerMenu(nav, 'nav_active', '.nav__btn');
+
 export const renderNavigation = () => {
 
   nav.textContent = '';
@@ -14,7 +20,42 @@ export const renderNavigation = () => {
   });
 
   buttonSignUp.addEventListener('click', () => {
-    console.log('Зарегистрироваться')
+    renderModal({
+      title: 'Регистрация',
+      description: 'Введите ваши данные для регистрации на сервисе WishList',
+      btnSubmit: 'Зарегистрироваться',
+      async submitHandler (event) {
+        const formData = new FormData(event.target);
+        const credentials = {
+          login: formData.get('login'),
+          password: formData.get('password'),
+        };
+
+        try {
+          const response = await fetch(`${API_URL}/register`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(credentials)
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem(JWT_TOKEN_KEY, data.token);
+            auth.login = data.login;
+            router.setRoute(`/user/${data.login}`)
+
+            return true;
+
+          } else {
+            const { message = 'Неизвестная ошибка' } = await response.json();
+            console.log(await response.json());
+            throw new Error('Invalid credentials');
+          }
+        } catch (error) {
+          alert(error.message);
+        }
+      }
+    })
   })
 
   const buttonSignIn = createElement('button', {
